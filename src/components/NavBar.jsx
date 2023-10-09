bc4ea47992e2e5a4e3346333e4fee41345fa413d
Bryan Balderrama <bryxn.alex77@gmail.com>
2023-10-09 12:51:34 -0600

    First commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import house from '../assets/cil_house.svg';
+
+export const NavBar = () => {
+
+    return (
+        <>
+            <nav className="bg-[#FD9255] h-[196px]">
+                <img src={house} />
+                <h1>NavBar</h1>
+
+            </nav>
+        </>
+    );
+
+}
